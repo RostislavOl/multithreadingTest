24210fa5d7346690dd990b9069324a751ea71d79
RostislavOl <zonaTostATY@gmail.com>
2021-08-27 14:50:42 +0400

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,89 @@
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Insert the file path for search:");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+        System.out.println("Insert file extensions:");
+        String extension = sc.nextLine();
+        File dir = new File(path);
+
+        ArrayList<String> result = new ArrayList<>();
+
+        Queue<File> fileTree = new PriorityQueue<>();
+
+        Collections.addAll(fileTree, Objects.requireNonNull(dir.listFiles()));
+
+        while (!fileTree.isEmpty()) {
+            File currentFile = fileTree.remove();
+            if (currentFile.isDirectory()) {
+                Collections.addAll(fileTree, Objects.requireNonNull(currentFile.listFiles()));
+            } else {
+                result.add(currentFile.getAbsolutePath());
+            }
+        }
+        ArrayList<String> fin = new ArrayList<>();
+
+        ArrayList<Future<String>> results =
+                new ArrayList<Future<String>>();
+
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++)
+            results.add(exec.submit(new TaskWithResult(extension, fin, result)));
+        for(Future<String> fs : results)
+            try {
+                // Вызов get() блокируется до завершения;:
+                System.out.println(fs.get());
+            } catch(InterruptedException e) {
+                System.out.println(e);
+                return;
+            } catch(ExecutionException e) {
+                System.out.println(e);
+            } finally {
+                exec.shutdown();
+            }
+
+    }
+
+
+}
+
+class TaskWithResult implements Callable<String> {
+
+    public String extension;
+
+    public ArrayList<String> fin;
+
+    public ArrayList<String> res;
+
+    TaskWithResult(String extension, ArrayList<String> fin, ArrayList<String> res) {
+        this.extension = extension;
+        this.fin = fin;
+        this.res = res;
+    }
+
+    ArrayList<String> getExtFiles(String extension, List<String> files) {
+        synchronized (files) {
+            for (String fileString : files) {
+                String ext = FilenameUtils.getExtension(fileString);
+                if (extension.equals(ext)) {
+                    fin.add(fileString);
+                }
+            }
+            return fin;
+        }
+    }
+
+    @Override
+    public String call() throws Exception {
+        return getExtFiles(extension, res).toString();
+    }
+}
